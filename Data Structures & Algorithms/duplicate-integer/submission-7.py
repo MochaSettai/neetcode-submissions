89a1213d1a00568ccb4d55b76dbd7faee5511468
Surya Kiran <161462462+MochaSettai@users.noreply.github.com>
2026-04-03 14:32:02 +0100

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,18 @@
+'''
+217. Contains Duplicate
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+'''
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+
+            hashset.add(i)
+
+        return False
